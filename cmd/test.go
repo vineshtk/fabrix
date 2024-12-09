0d2a3420c6b8e891a6228a4548b6d1ae1c50d498
vineshtk <vineshtk10@gmail.com>
2024-12-09 23:31:25 +0530

    test

--- cmd/test.go
+++ cmd/test.go
@@ -2,12 +2,8 @@ package cmd
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/spf13/cobra"
-
-	"github.com/charmbracelet/bubbles/textinput"
-	tea "github.com/charmbracelet/bubbletea"
 )
 
 var testCmd = &cobra.Command{
@@ -16,66 +12,8 @@ var testCmd = &cobra.Command{
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command.`,
 	Run: func(cmd *cobra.Command, args []string) {
-
-		p := tea.NewProgram(initialModel())
-		if _, err := p.Run(); err != nil {
-			log.Fatal(err)
-		}
+fmt.Println("test all codes here")
 	},
-}
-
-type (
-	errMsg error
-)
-
-type model struct {
-	textInput textinput.Model
-	err       error
-}
-
-func initialModel() model {
-	ti := textinput.New()
-	ti.Placeholder = "Number of organizations?"
-	ti.Focus()
-	ti.CharLimit = 156
-	ti.Width = 20
-
-	return model{
-		textInput: ti,
-		err:       nil,
-	}
-}
-
-func (m model) Init() tea.Cmd {
-	return textinput.Blink
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
-
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyEnter, tea.KeyCtrlC, tea.KeyEsc:
-			return m, tea.Quit
-		}
-
-	// We handle errors just like any other message
-	case errMsg:
-		m.err = msg
-		return m, nil
-	}
-
-	m.textInput, cmd = m.textInput.Update(msg)
-	return m, cmd
-}
-
-func (m model) View() string {
-	return fmt.Sprintf(
-		"What’s the domain name?\n\n%s\n\n%s",
-		m.textInput.View(),
-		"(esc to quit)",
-	) + "\n"
 }
 
 func init() {
